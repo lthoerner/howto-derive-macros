76a18c608a9c9218d78309afddd67abfd9e8e073
lthoerner <lowellashton@gmail.com>
2024-12-12 13:49:16 -0800

    Step 10: adding additional trait member that will require a custom attribute

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use my_macros::Identify;
 
 trait Identify {
     fn type_name(&self) -> &'static str;
+    fn friendly_type_name(&self) -> &'static str;
 }
 
 #[derive(Identify)]
@@ -14,5 +15,7 @@ fn main() {
     let my_bar = BarType;
 
     println!("{}", my_foo.type_name());
+    println!("{} :)", my_foo.friendly_type_name());
     println!("{}", my_bar.type_name());
+    println!("{} :)", my_bar.friendly_type_name());
 }
