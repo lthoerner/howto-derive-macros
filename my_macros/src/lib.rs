4e3733a0735c3dffc87ed1830c218837ae9ebb0b
lthoerner <lowellashton@gmail.com>
2024-12-12 13:34:49 -0800

    Step 6: adding the code-in-code-out function skeleton for the derive macro

--- my_macros/src/lib.rs
+++ my_macros/src/lib.rs
@@ -2,3 +2,8 @@ use proc_macro::TokenStream;
 // use proc_macro2::TokenStream as TokenStream2; (OPTIONAL)
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Identify)]
+pub fn derive_identify(input: TokenStream) -> TokenStream {
+    todo!()
+}
