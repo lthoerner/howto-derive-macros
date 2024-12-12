82104e8ca7af4316284247b3d42bfe9e7a7be8c5
lthoerner <lowellashton@gmail.com>
2024-12-12 13:17:02 -0800

    Step 1: adding the trait and attempting to use it without implementing it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+trait Identify {
+    fn type_name(&self) -> &'static str;
+}
+
+struct FooType;
+struct BarType;
+
 fn main() {
-    println!("Hello, world!");
+    let my_foo = FooType;
+    let my_bar = BarType;
+
+    println!("{}", my_foo.type_name());
+    println!("{}", my_bar.type_name());
 }
