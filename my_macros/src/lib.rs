245f2e3a9cbdbd212a11c58a3b5864ca2292a388
lthoerner <lowellashton@gmail.com>
2024-12-12 14:09:56 -0800

    Step 15: parsing the `friendly_name` attribute into a string and outputting it with `quote`

--- my_macros/src/lib.rs
+++ my_macros/src/lib.rs
@@ -10,15 +10,20 @@ struct FriendlyNameAttribute(String);
 
 #[proc_macro_derive(Identify, attributes(friendly_name))]
 pub fn derive_identify(input: TokenStream) -> TokenStream {
-    let DeriveInput {
-        ident: type_name, ..
-    } = parse_macro_input!(input);
-    let type_name_string = type_name.clone().to_string();
+    let mut input: DeriveInput = parse_macro_input!(input);
+    let type_name = input.ident.clone();
+    let type_name_string = input.ident.clone().to_string();
+    let FriendlyNameAttribute(friendly_name_string) =
+        deluxe::extract_attributes(&mut input).unwrap();
 
     quote! {
         impl crate::Identify for #type_name {
             fn type_name(&self) -> &'static str {
                 #type_name_string
+            }
+
+            fn friendly_type_name(&self) -> &'static str {
+                #friendly_name_string
             }
         }
     }
